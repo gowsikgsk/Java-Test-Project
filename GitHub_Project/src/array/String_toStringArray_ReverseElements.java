cc2ca560a513e8a0cd6b3c6a69464668d5d2e6ef
Gowsikgsk <Gowsik92@rediffmail.com>
2021-01-03 17:21:28 +0530

    Updated with  Reverse String

--- GitHub_Project/src/array/String_toStringArray_ReverseElements.java
+++ GitHub_Project/src/array/String_toStringArray_ReverseElements.java
@@ -2,7 +2,7 @@ package array;
 public class String_toStringArray_ReverseElements {
 	/***************** program to convert String To String Array without using Spilt Function********************/ 
 	static void rev() {
- 	   String S = "i love java"; 
+ 	   String S = "i love java "; 
  	   String temp="";
  	   int counter =0;
  	   char cc[]=S.toCharArray();
@@ -14,7 +14,7 @@ public class String_toStringArray_ReverseElements {
          	}
    		}
  	   
- 	   int j=0;
+ 	   int j=0; //initilze j for String Array Increment 
  	   String G[] = new String[counter+1];// space +1=word present i the String
  	   for (int k=0;k<=cc.length-1;k++) 
  	   { 
@@ -34,34 +34,52 @@ public class String_toStringArray_ReverseElements {
    	    { 
          	//System.out.print(G[x]); // to print the String Array G[]
    		}  
- /*****************To Reverse Element ********************/ //reverse not working fully 	   
+ 	   
+ /*****************To Reverse Element ********************/   
+ 	   
  	   char cr[]=new char[cc.length]; // declare cr array length (cc.length)
- 	   
- 	   for(int i=0;i<G.length;i++)
- 	   {
- 	      cr=G[i].toCharArray(); // G[o] convert to char array
- 	      for(int s=0;s<G[i].length()/2;s++) // Transverse 
- 	      {
- 	    	  char temper=cr[s];
- 	    	  cr[s]=cr[G[i].length()-s-1];
- 	    	 cr[G[i].length()-s-1]=temper;
- 	      } //for loop s
- 	   
- 	   }//for loop i
- 	   
- 	  for(int i=0;i<cr.length;i++)
-	   {
- 		  System.out.print(cr[i]); //to print char array cr 
-	   }
- 	   
+ 	    String SG=""; // to store reversed array 
+     	for(int k=0;k<G.length;k++)
+     	{
+     	cr=G[k].toCharArray(); // convert every String Array iteration to char Array 
+     	
+     	for (int i=G[k].length()-1;i>=0;i--) //  decrement i value
+     	 {
+     	for (int s=0;s<=G[k].length();s++)  //  increment s value 
+     	 {
+     	if(s==cr.length-i-1)   // this condition ensure  j i  04  13  22  31  40 to reverse array
+     	 {
+     	SG=SG+cr[i];
+     	  }//if 
+     	  }// s for 2nd inner
+     	  } // i for 1st inner
+     	  }//k for outer for 
+	  
+     	  System.out.println(SG); 
+     	 System.out.println("Space element also Reversed ");
     }
-
 	public static void main(String[] args) {
 		rev(); // method calling 
-
 	}
-
 }
+
+/*----------------------------Work-out codes-----------------------------------------------------------*/
+/*for(int i=0;i<G.length;i++)
+ {
+    cr=G[i].toCharArray(); // G[o] convert to char array
+    for(int s=0;s<G[i].length()/2;s++) // Transverse 
+    {
+  	  char temper=cr[s];
+  	  cr[s]=cr[G[i].length()-s-1];
+  	 cr[G[i].length()-s-1]=temper;
+    } //for loop s
+ 
+ }//for loop i
+ 
+for(int i=0;i<cr.length;i++)
+{
+	  System.out.print(cr[i]); //to print char array cr 
+}*/
 
 /*  for(int g=G[i].length()-1;g>=0;g--)
   {
