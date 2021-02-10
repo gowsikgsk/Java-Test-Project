200450be53d2078f99561daa3cfc2955b982d4ff
Karupu <Karupu@DESKTOP-ASLEK2D>
2021-02-10 20:59:41 +0530

    first commit

--- GitHub_Project/src/array/Length_Array_Element.java
+++ GitHub_Project/src/array/Length_Array_Element.java
@@ -1,10 +1,25 @@
 package array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Length_Array_Element {
+	void FindLength() {
+		String SS="";
+		 String empty="";
+		 ArrayList<String> ar=new ArrayList<String>();
+		 String D[]= {"gowsik","g","f","life",""};
+		 ar.addAll(Arrays.asList(D));
+		 for(int k=0;k<ar.size();k++) 
+	        { 
+		      System.out.println(D[k]+" = "+D[k].lastIndexOf(empty)+" ");
+		  //  SS=ar.get(k).toString();
+	        }
+	}// FindLength()
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Length_Array_Element len=new Length_Array_Element();
+		 len.FindLength();
 	}
 
 }
